2e64f9e93476e974393296e5d038012c4132a481
cppizarro <cppizarro@miuandes.cl>
2021-03-24 15:53:34 -0300

    tal vez definitivo

--- Lab01.py
+++ Lab01.py
@@ -1,51 +1,52 @@
 #Al jugar las coordenadas se escriben sin los parentesis
+#Se asume que lo que ingrese el usuario va a ser correcto (no 
+# va a poner coordenadas donde ya no hay cartas, coordenadas 
+# que no existan o la misma coordenada dos veces)
 
 import numpy as np
 from tabulate import tabulate
 
-number_of_cards = int(input("Numero de cartas a jugar: "))
+######## GENERADOR DE CARTAS Y TABLEROS ########
+def boards(number_of_cards):
+    cards = []
+    for i in range(1, number_of_cards + 1):
+        cards.append(i)
+        cards.append(i)
+    np.random.shuffle(cards)
 
-cards = []
-for i in range(1, number_of_cards + 1):
-    cards.append(i)
-    cards.append(i)
+    columns = 4
+    rows = (number_of_cards*2)/columns
 
-np.random.shuffle(cards)
+    board1 = []
+    i = 0
+    j = 0
+    while j < rows:
+        row = []
+        k = 0
+        while i < len(cards) and k < columns:
+            f = str(j)
+            c = str(k)
+            row.append((j,k))
+            i += 1
+            k += 1
+        board1.append(row)
+        j += 1
 
-board1 = []
-board2 = []
+    board2 = []
+    i = 0
+    j = 0
+    while j < rows:
+        row = []
+        k = 0
+        while i < len(cards) and k < columns:
+            row.append(cards[i])
+            i += 1
+            k += 1
+        board2.append(row)
+        j += 1
+      return board1, board2
 
-columns = 4
-rows = (number_of_cards*2)/columns
-
-i = 0
-j = 0
-while j < rows:
-    row = []
-    k = 0
-    while i < len(cards) and k < columns:
-        f = str(j)
-        c = str(k)
-        row.append((j,k))
-        i += 1
-        k += 1
-    board1.append(row)
-    j += 1
-
-i = 0
-j = 0
-while j < rows:
-    row = []
-    k = 0
-    while i < len(cards) and k < columns:
-        row.append(cards[i])
-        i += 1
-        k += 1
-    board2.append(row)
-    j += 1
-
-#################################################
-
+################    JUEGO    ################
 k = 0
 points = {"Jugador 1":0, "Jugador 2":0 }
 def game(board1,board2,k,points):
@@ -55,13 +56,16 @@ def game(board1,board2,k,points):
     while n < len(board1):
         i = board1[n]
         if all(type(j) is str for j in i):
-            print(points)
             if points["Jugador 1"] > points["Jugador 2"]:
-                print("\n", "El ganador es: Jugador 1")
+                x = 0
             else:
-                print("\n", "El ganador es: Jugador 2")
+                x = 1
             n += 1
             if n == len(board1):
+                if x == 0:
+                    print("\n", "El ganador es: Jugador 1")
+                if x == 1:
+                    print("\n", "El ganador es: Jugador 2")
                 return
         else:
             break
@@ -95,18 +99,18 @@ def game(board1,board2,k,points):
         if k == 0:
             k = 0
         elif k == 1:
-            k = 1
-        game(board1, board2, k, points)
-        
-    if card1 != card2:
+            k = 1   
+    elif card1 != card2:
         board1[x1][y1] = ((x1,y1))
         board1[x2][y2] = ((x2,y2))
         if k == 0:
             k = 1
         elif k == 1:
             k = 0
-        game(board1, board2, k, points)
-    
+    game(board1, board2, k, points)
 
-print(tabulate(board2))
+###########################################################
+number_of_cards = int(input("Numero de cartas a jugar: "))
+board1 = boards(number_of_cards)[0]
+board2 = boards(number_of_cards)[1]
 game(board1,board2,k,points)
